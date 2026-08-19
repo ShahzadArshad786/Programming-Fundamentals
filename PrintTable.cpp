65bfeea05cd7872e32349457a11ed242473f3159
Your Name <you@example.com>
2026-08-19 13:03:29 +0500

    First Commit

---
+++ PrintTable.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ; 
+
+int main()
+{
+
+    int num ; 
+
+    cout<<"Enter an Integer Number for a Table : ";
+    cin>>num ; 
+
+    for(int i = 1 ; i <= 10 ; i++)
+    {
+        cout<<i<< " * "<< num << " = "<< i * num<<endl; 
+    }
+
+    return 0 ; 
+}
