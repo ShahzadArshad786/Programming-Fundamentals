6569b4998959c6de3a6f153e02310c2dc40c073c
Your Name <you@example.com>
2026-08-19 14:37:02 +0500

    First Commit

--- PrintTable.cpp
+++ PrintTable.cpp
@@ -3,6 +3,19 @@
 
     Write a program using C++ that inputs an positive number and prints table of that input number using any loop. 
 
+    If user input 5, the output will be displayed as:
+
+    5 * 1 = 5
+    5 * 2 = 10
+    5 * 3 = 15
+    5 * 4 = 20
+    5 * 5 = 25
+    5 * 6 = 30
+    5 * 7 = 35
+    5 * 8 = 40
+    5 * 9 = 45
+    5 * 10 = 50
+
 */
 
 #include<iostream>
