f57ff0bc7f2bf3c7ed038fabe88cf9e2226b4177
Your Name <you@example.com>
2026-08-19 12:54:05 +0500

    First Commit

--- Factorial.cpp
+++ Factorial.cpp
@@ -5,7 +5,17 @@ using namespace std ;
 int main()
 {
 
+    int num , fact = 1 ; 
 
-    cout<<"Welcome to KIT"<<endl; 
+    cout<<"Enter an integer value : " ;
+    cin>>num ; 
+
+    for(int i = 1 ; i <= num ; i++)
+    {
+        fact = fact * i ; 
+    }
+
+    cout<<"Factotial of "<<num<< " is : "<<fact<<endl; 
+     
     return 0 ; 
 }
