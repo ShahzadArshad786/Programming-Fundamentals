33d9f89561254a21e7fdf6a459e68625a549cdec
Your Name <you@example.com>
2026-08-20 10:41:40 +0500

    First Commit

--- EvenNumbersBetweenRange.cpp
+++ EvenNumbersBetweenRange.cpp
@@ -1,5 +1,14 @@
 
+/*
+    Write a program that inputs ranges from the user and displays all even numbers between the given range. 
 
+    For Example, if user enter 5 as start of range, and 21 as end of range, then display all even numbers 
+    between 5 and 21. 
+
+    Output will be: 
+
+    Even Numbers Between 5 and 21 are : 6 8 10 12 14 16 18 20
+*/
 
 
 #include<iostream>
